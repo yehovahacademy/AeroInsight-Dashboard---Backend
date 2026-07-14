1a1a73d2f8f019f3e9221fc2785dabebe409c06a
yehovahacademy <yehovahacademy@gmail.com>
2026-07-14 11:09:45 +0530

    Load flight data from JSON and implement flight filtering

--- app/routes/flights.py
+++ app/routes/flights.py
@@ -1,9 +1,23 @@
+from typing import Optional
+
 from fastapi import APIRouter
+
 from app.services.flight_service import get_all_flights
 from app.schemas.flight_schema import Flight
 
 router = APIRouter()
 
+
 @router.get("/", response_model=list[Flight])
-def list_flights():
-    return get_all_flights()
+def list_flights(
+    airline: Optional[str] = None,
+    origin: Optional[str] = None,
+    destination: Optional[str] = None,
+    status: Optional[str] = None,
+):
+    return get_all_flights(
+        airline=airline,
+        origin=origin,
+        destination=destination,
+        status=status,
+    )
--- app/services/flight_service.py
+++ app/services/flight_service.py
@@ -1,5 +1,59 @@
-from app.config.flights import FLIGHTS
+import json
+from pathlib import Path
 
-def get_all_flights():
-    return FLIGHTS
-    
+DATA_FILE = Path(__file__).parent.parent / "data" / "flights.json"
+
+def load_flights():
+    print(f"Reading file: {DATA_FILE}")
+    print(f"Exists: {DATA_FILE.exists()}")
+
+    with open(DATA_FILE, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    print("First 100 characters:")
+    print(repr(content[:100]))
+
+    return json.loads(content)
+
+
+def get_all_flights(
+    airline: str | None = None,
+    origin: str | None = None,
+    destination: str | None = None,
+    status: str | None = None
+):
+    """
+    Return flights with optional filters.
+    """
+
+    flights = load_flights()
+
+    if airline:
+        flights = [
+            flight
+            for flight in flights
+            if flight["airline"].lower() == airline.lower()
+        ]
+
+    if origin:
+        flights = [
+            flight
+            for flight in flights
+            if flight["origin"].lower() == origin.lower()
+        ]
+
+    if destination:
+        flights = [
+            flight
+            for flight in flights
+            if flight["destination"].lower() == destination.lower()
+        ]
+
+    if status:
+        flights = [
+            flight
+            for flight in flights
+            if flight["status"].lower() == status.lower()
+        ]
+
+    return flights
