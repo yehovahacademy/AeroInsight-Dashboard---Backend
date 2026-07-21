e37bc4c1765704af03c382ae33514606d79f2272
yehovahacademy <yehovahacademy@gmail.com>
2026-07-21 09:13:20 +0530

    New changes

--- app/main.py
+++ app/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
 from app.routes.airlines import router as airline_router
 from app.routes.flights import router as flight_router
 from app.routes.analytics import router as analytics_router
 
+
 app = FastAPI(
 title="My FastAPI Application"
 )
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "https://aero-insight-dashboard-frontend.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(airline_router, prefix="/airlines", tags=["Airlines"])
 
