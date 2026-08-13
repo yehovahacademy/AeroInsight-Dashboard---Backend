07655ce3d6b1954ceca765dac0a9efd147c8285b
yehovahacademy <yehovahacademy@gmail.com>
2026-08-13 11:45:03 +0530

    changing data sources and removing unnecessary files

--- app/routes/network.py
+++ app/routes/network.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter
 from app.schemas.network_schema import RouteRequest, RouteAnalysis
 from app.services.network_service import analyze_route
 
-
-router = APIRouter()
+router = APIRouter(
+    prefix="/api/network",
+    tags=["Network Planning"]
+)
 
 
 @router.post("/analyze_route", response_model=RouteAnalysis)
 def analyze_route_endpoint(request: RouteRequest):
-
     return analyze_route(request)
