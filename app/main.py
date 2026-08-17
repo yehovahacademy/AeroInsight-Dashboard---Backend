bd55c80a8641aed816492e784562ac22f837544b
yehovahacademy <yehovahacademy@gmail.com>
2026-08-17 18:58:33 +0530

    Resolved the CORS error forgot to add the databse Localhost

--- app/main.py
+++ app/main.py
@@ -42,6 +42,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://aero-insight-dashboard-frontend.vercel.app"
+        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
