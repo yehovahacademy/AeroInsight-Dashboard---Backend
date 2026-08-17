6839b0313f4e7c70ea56d2d038f95539c17093ed
yehovahacademy <yehovahacademy@gmail.com>
2026-08-17 22:06:28 +0530

    Currently resolving CORS issue

--- app/main.py
+++ app/main.py
@@ -39,14 +39,12 @@ def test_routes():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://your-vercel-frontend.vercel.app",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(airline_router, prefix="/airlines", tags=["Airlines"])
 app.include_router(route_router, prefix="/routes", tags=["Routes"])
