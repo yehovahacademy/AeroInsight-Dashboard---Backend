fb9b1528d3f843d3c18eab5d713ce4978ca2e472
yehovahacademy <yehovahacademy@gmail.com>
2026-08-17 19:00:16 +0530

    Setting the credentials to False

--- app/main.py
+++ app/main.py
@@ -39,12 +39,8 @@ def test_routes():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://aero-insight-dashboard-frontend.vercel.app"
-        "http://127.0.0.1:5173",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
