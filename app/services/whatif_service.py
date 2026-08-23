e687812efe5d9e5bf1a8047a604c90c895a0b744
yehovahacademy <yehovahacademy@gmail.com>
2026-08-24 00:10:48 +0530

    500 server error resolved from backend

---
+++ app/services/whatif_service.py
@@ -0,0 +1,17 @@
+def calculate_what_if(request):
+
+    capacity = get_aircraft_capacity(request.aircraft)
+
+    passengers = capacity * request.load_factor
+
+    revenue_per_flight = passengers * request.average_fare
+
+    daily_revenue = revenue_per_flight * request.flights_per_day
+
+    return {
+        "aircraft": request.aircraft,
+        "capacity": capacity,
+        "estimated_passengers": round(passengers),
+        "revenue_per_flight": round(revenue_per_flight),
+        "daily_revenue": round(daily_revenue)
+    }
