f09df99ddbe6674078f1b3f3d9f0deaec604bde4
yehovahacademy <yehovahacademy@gmail.com>
2026-09-14 12:47:59 +0530

    competiton repo is updated and debugged for 500 server error

--- app/repositories/competition_repository.py
+++ app/repositories/competition_repository.py
@@ -77,6 +77,36 @@ class CompetitionRepository:
 
                 return cursor.fetchall()
 
+    def get_by_market_and_year(
+    self,
+    market_id: str,
+    year: int
+):
+     with get_connection() as connection:
+        with connection.cursor(
+            cursor_factory=psycopg2.extras.RealDictCursor
+        ) as cursor:
+
+            cursor.execute("""
+                SELECT
+                    competition_id,
+                    market_id,
+                    year,
+                    airline_name,
+                    is_direct_competitor,
+                    frequency_per_week,
+                    aircraft_type,
+                    market_share,
+                    competitive_strength,
+                    data_type
+                FROM public.competition_new
+                WHERE market_id = %s
+                AND year = %s
+                ORDER BY market_share DESC
+            """, (market_id, year))
+
+            return cursor.fetchall()        
+
     def get_by_airline(self, airline_name: str):
         with get_connection() as connection:
             with connection.cursor(
