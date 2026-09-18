d10976afb44e77178cb472634f8bbb2c93a07df7
yehovahacademy <yehovahacademy@gmail.com>
2026-09-18 11:35:57 +0530

    Database debugging completed

--- app/database.py
+++ app/database.py
@@ -3,21 +3,12 @@ import psycopg2
 from contextlib import contextmanager
 from dotenv import load_dotenv
 
-print("Before load_dotenv:", os.getenv("DATABASE_URL"))
 
 load_dotenv()
 
-print("Afetr load_dotenv:", os.getenv("DATABASE_URL"))
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL:
-    print(
-        "DATABASE HOST:",
-        DATABASE_URL.split("@")[-1].split("/")[0]
-    )
-else:
-    print("DATABASE_URL is NOT SET")
 
 
 @contextmanager
